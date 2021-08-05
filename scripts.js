b5b70a10798df97a1b784c6c113de37936d77bca
TabulateJarl8 <tabulatejarl8@gmail.com>
2021-08-05 11:39:07 -0400

    beautify code

--- scripts.js
+++ scripts.js
@@ -1,7 +1,7 @@
 function setCookie(name, value, daysToLive) {
 	var cookie = name + "=" + encodeURIComponent(value);
-	if(typeof daysToLive === "number") {
-		cookie += "; max-age=" + (daysToLive*24*60*60);
+	if (typeof daysToLive === "number") {
+		cookie += "; max-age=" + (daysToLive * 24 * 60 * 60);
 		cookie += "; path=/";
 		document.cookie = cookie;
 	}
@@ -9,9 +9,9 @@ function setCookie(name, value, daysToLive) {
 
 function getCookie(name) {
 	var cookieArr = document.cookie.split(";");
-	for(var i = 0; i < cookieArr.length; i++) {
+	for (var i = 0; i < cookieArr.length; i++) {
 		var cookiePair = cookieArr[i].split("=");
-		if(name == cookiePair[0].trim()) {
+		if (name == cookiePair[0].trim()) {
 			return decodeURIComponent(cookiePair[1]);
 		}
 	}
@@ -20,28 +20,28 @@ function getCookie(name) {
 
 function checkCookie(name) {
 	var value = getCookie(name);
-	if(value == null) {
+	if (value == null) {
 		return false;
 	} else {
 		return true;
 	}
 }
 
-function deleteCookie(name){
+function deleteCookie(name) {
 	document.cookie = name += "=; max-age=0";
 }
 
 function getUrlVars() {
 	var vars = {};
-	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+	var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m, key, value) {
 		vars[key] = value;
 	});
 	return vars;
 }
 
-function getUrlParam(parameter, defaultvalue){
+function getUrlParam(parameter, defaultvalue) {
 	var urlparameter = defaultvalue;
-	if(window.location.href.indexOf(parameter) > -1){
+	if (window.location.href.indexOf(parameter) > -1) {
 		urlparameter = getUrlVars()[parameter];
 	}
 	return urlparameter;
@@ -51,13 +51,13 @@ function signOut(redirect) {
 	if (checkCookie("username")) {
 		deleteCookie("username");
 		deleteCookie("token");
-		if(redirect != false){
+		if (redirect != false) {
 			window.location.href = "/login?redirect=" + redirect;
 		}
 	} else if (sessionStorage.getItem("username") != null) {
 		sessionStorage.removeItem("username");
 		sessionStorage.removeItem("token");
-		if(redirect != false){
+		if (redirect != false) {
 			window.location.href = "/login?redirect=" + redirect;
 		}
 
@@ -66,54 +66,57 @@ function signOut(redirect) {
 $.extend({
 	xResponse: function(token, user) {
 		var responseText = null;
-		$.ajax(
-			{
-				url: 'https://tabulateflask.azurewebsites.net/checklogin',
-				type: 'POST',
-				dataType: 'text',
-				async: false,
-				data: {token: token, name: user},
-				success: function(response)
-				{
-					responseText = response;
-				}
-			});
-			return responseText;
-		}
-	});
+		$.ajax({
+			url: 'https://tabulateflask.azurewebsites.net/checklogin',
+			type: 'POST',
+			dataType: 'text',
+			async: false,
+			data: {
+				token: token,
+				name: user
+			},
+			success: function(response) {
+				responseText = response;
+			}
+		});
+		return responseText;
+	}
+});
 
-	function checkLogin(){
-		if(checkCookie("token")){
-			if(checkCookie("username")){
-				var check = $.xResponse(getCookie("token"), getCookie("username"));
-				if(check != "false"){
-					setCookie("token", check, 30);
-					return true;
-				}else{
-					return false;
-				}
-			}else{
+function checkLogin() {
+	if (checkCookie("token")) {
+		if (checkCookie("username")) {
+			var check = $.xResponse(getCookie("token"), getCookie("username"));
+			if (check != "false") {
+				setCookie("token", check, 30);
+				return true;
+			} else {
 				return false;
 			}
-		}else if(sessionStorage.getItem("token") != null){
-			if(sessionStorage.getItem("username") != null){
-				var check = $.xResponse(sessionStorage.getItem("token"), sessionStorage.getItem("username"));
-				if(check != "false"){
-					sessionStorage.removeItem("token");
-					sessionStorage.setItem("token", check);
-					return true;
-				}else{
-					return false;
-				}
-			}else{
-				return false;
-			}
-		}else{
+		} else {
 			return false;
 		}
+	} else if (sessionStorage.getItem("token") != null) {
+		if (sessionStorage.getItem("username") != null) {
+			var check = $.xResponse(sessionStorage.getItem("token"), sessionStorage.getItem("username"));
+			if (check != "false") {
+				sessionStorage.removeItem("token");
+				sessionStorage.setItem("token", check);
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			return false;
+		}
+	} else {
+		return false;
 	}
+}
 window.mobileCheck = function() {
-  let check = false;
-  (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-  return check;
+	let check = false;
+	(function(a) {
+		if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true;
+	})(navigator.userAgent || navigator.vendor || window.opera);
+	return check;
 };
